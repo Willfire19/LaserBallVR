3ad4fd054b0af7e89e65a09c7fc690e99555ec08
Joshua Angeley <jda5dp@virginia.edu>
2015-12-12 22:22:30 -0500

    Added A Simple Gate
    When the Player enters the Gate, the text 'You Won' will display in the
    logs.

---
+++ Tutorial/Assets/Scripts/GateCollide.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GateCollide : MonoBehaviour {
+
+    //GameObject gate;
+	// Use this for initialization
+	void Start () {
+        //gate = gameObject;
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if( other.gameObject.name == "Player")
+        {
+            Debug.Log("You Won!");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
