44ad875f9acfaec4c8aa3ff63915c804d37fbb4f
Joshua Angeley <jda5dp@virginia.edu>
2016-01-05 23:01:40 -0500

    Added Photon for Basic Multiplayer
    Only the player position and the player head are being tracked and sent
    over the internet.

---
+++ Tutorial/Assets/Scripts/NetworkedPlayer.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkedPlayer : Photon.MonoBehaviour
+{
+	public GameObject avatar;
+
+	public Transform playerGlobal;
+	public Transform playerLocal;
+
+	void Start ()
+	{
+		Debug.Log("i'm instantiated");
+
+		if (photonView.isMine)
+		{
+			Debug.Log("player is mine");
+
+			playerGlobal = GameObject.Find("Player").transform;
+			playerLocal = playerGlobal.Find("Main Camera");
+
+			this.transform.SetParent(playerLocal);
+			this.transform.localPosition = Vector3.zero;
+
+			// avatar.SetActive(false);
+		}
+	}
+
+	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		if (stream.isWriting)
+		{
+			stream.SendNext(playerGlobal.position);
+			stream.SendNext(playerGlobal.rotation);
+			stream.SendNext(playerLocal.localPosition);
+			stream.SendNext(playerLocal.localRotation);
+		}
+		else
+		{
+			this.transform.position = (Vector3)stream.ReceiveNext();
+			this.transform.rotation = (Quaternion)stream.ReceiveNext();
+			avatar.transform.localPosition = (Vector3)stream.ReceiveNext();
+			avatar.transform.localRotation = (Quaternion)stream.ReceiveNext();
+		}
+	}
+}
