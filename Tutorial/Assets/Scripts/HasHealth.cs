f0beee47fb8394a2a78b78e4bfe031fe5768195b
Joshua Angeley <jda5dp@virginia.edu>
2016-03-16 00:24:08 -0400

    ReAdded all my changed scripts for Player Death

--- Tutorial/Assets/Scripts/HasHealth.cs
+++ Tutorial/Assets/Scripts/HasHealth.cs
@@ -5,15 +5,49 @@ public class HasHealth : MonoBehaviour {
 
 	public float hitPoints = 100f;
 
+	[PunRPC]
 	public void RecieveDamage( float hit_amount ){
 		hitPoints -= hit_amount;
+		if (gameObject.CompareTag("NetworkedPlayer")) {
+			Debug.Log ("NeworkedPlayer is taking damage!");
+			if( GetComponent<PhotonView>().isMine ){
+				GameObject.FindGameObjectWithTag ("Player").GetComponent<HasHealth> ().RecieveDamage (hit_amount);
+			}
+		}
 		if( hitPoints <= 0 ){
 			Die();
 		}
 	}
 
 	public void Die(){
+
+		// Game crashes when Player dies. The Player gameObject gets destroyed.
+
 		// DeathAnimation()
-		Destroy (gameObject);
+		Debug.Log (gameObject.name + " died!");
+		// Check if Photon Network Instantiated this object
+		if (GetComponent<PhotonView> ().instantiationId == 0) {			
+			if (!gameObject.CompareTag ("Player")) {
+				Destroy (gameObject);
+			}
+		} else {
+			// This means that the NetworkedPlayer is dead
+			// PhotonNetwork.Destroy( GetComponent<PhotonView>() );
+
+			// Check if the dead person is the player
+			if (gameObject.CompareTag ("Player")) {
+				gameObject.GetComponent<FirstPersonController> ().Die ();
+			} else {
+				// This is the NetworkedPlayer
+				Debug.Log(gameObject.name + " will be deactivated");
+				//gameObject.SetActive(false);
+				gameObject.GetComponent<NetworkedPlayer>().Die();
+			}
+		} 
+
+	}
+
+	public void Heal(float healthPoints){
+		hitPoints = healthPoints;
 	}
 }
